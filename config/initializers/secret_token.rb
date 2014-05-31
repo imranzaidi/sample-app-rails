979fd7266a7ca01cd815e8ff925fbb6183976e19
imranalizaidi <imran@salsamobi.com>
2014-05-31 18:58:13 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleAdd::Application.config.secret_token = '2b3024446390bf27a2b03920a3e7e2e025222d448784ec9cf9cde218ba5aa6d2192a0a755ea10433ed0f31c8baf5ffdaeb26d0c0b6a9a54fbcd0a5f43dee7fdd'
