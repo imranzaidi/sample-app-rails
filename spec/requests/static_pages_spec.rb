c569ad3059ad9607fd785e705742fb832838a7d0
imranalizaidi <imran@salsamobi.com>
2014-08-02 17:57:08 -0500

    Tutorial complete up to 11.3, 4 unit tests failing, likely cause is compatibility between deviser and rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -31,6 +31,33 @@ describe "StaticPages" do
         end
       end
     end
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem")
+        FactoryGirl.create(:micropost, user: user, content: "Ipsum")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          page.should have_selector("li##{item.id}", text: item.content)
+        end
+      end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 follower", href: followers_user_path(user)) }
+      end
+    end
   end
 
   describe "Help Page" do
@@ -55,7 +82,7 @@ describe "StaticPages" do
   end
 
   # test home page links
-  it "shoud have the right links on the layout" do
+  it "should have the right links on the layout" do
     visit root_path
     click_link "About"
     page.should have_selector 'title', text: full_title('About Us')
